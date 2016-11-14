0d95aa037bda34b568b560a1e1726797eee7861c
Jon Maldia <jon@maldia.net>
2016-11-13 20:20:18 -0500

    Installed Devise

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,8 @@ module RestaurantappJon
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # Needed when setting up Devise
+    config.assets.initialize_on_precompile = false
   end
 end
