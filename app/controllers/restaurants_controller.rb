bd86a631447f8893ab9c86c03fb877ca988d7f02
Jon Maldia <jon@maldia.net>
2016-11-16 12:07:00 -0500

    Added description attribute to restaurant. Modified some Bootstrap CSS

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -71,6 +71,6 @@ class RestaurantsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def restaurant_params
-      params.require(:restaurant).permit(:name)
+      params.require(:restaurant).permit(:description, :name)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161114024317) do
+ActiveRecord::Schema.define(version: 20161116015326) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.integer  "user_id"
+    t.string   "description"
     t.index ["user_id"], name: "index_restaurants_on_user_id", using: :btree
   end
 
