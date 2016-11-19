631d0bacfebdd7715a36d3d271f00f767a34a659
Jon Maldia <jon@maldia.net>
2016-11-19 10:57:46 -0500

    Edited the homepage to show all restaurants. Edited the My Restaurants page to show only restaurants owned by the current user.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
   def home
+    if user_signed_in?
+      @restaurants = current_user.restaurants
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :restaurants
-  root 'pages#home'
-  get 'restaurants/index'
+  root 'restaurants#index'
+  get 'pages/home'
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
